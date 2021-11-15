bc8712243d32af32cc12f307b63aa4ceb54e636b
Veljko <veljomiha@gmail.com>
2021-11-15 09:25:00 +0100

    small change

--- src/ProductDetails.js
+++ src/ProductDetails.js
@@ -11,9 +11,9 @@ const ProductDetails = () =>{
                 <div className="psp-details">
                     <div className="psp-details1"><div className="psp-details11">Name</div> <div className="psp-details12">{products[id-1].name}</div></div>
                     <div className="psp-details1"><div className="psp-details11">Price</div> <div className="psp-details12">{products[id-1].price}€</div></div>
-                    <div className="psp-details1"><div className="psp-details11">RAM Memory</div> <div className="psp-details12">{products[id-1].ram}</div></div>
-                    <div className="psp-details1"><div className="psp-details11">Storage memory:</div> <div className="psp-details12">{products[id-1].memory}</div></div>
-                    <div className="psp-details1"><div className="psp-details11">Battery:</div> <div className="psp-details12">{products[id-1].battery}</div></div>
+                    <div className="psp-details1"><div className="psp-details11">RAM</div> <div className="psp-details12">{products[id-1].ram}</div></div>
+                    <div className="psp-details1"><div className="psp-details11">Storage memory</div> <div className="psp-details12">{products[id-1].memory}</div></div>
+                    <div className="psp-details1"><div className="psp-details11">Battery</div> <div className="psp-details12">{products[id-1].battery}</div></div>
 
                 </div>
                 
